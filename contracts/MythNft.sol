7b0d0644bfb7066871d389714e7e3a3cb03bc523
Fernando Cardozo <fhpcardozo@gmail.com>
2024-11-20 21:09:10 -0300

    feat: add MythNft contract

--- contracts/MythNft.sol
+++ contracts/MythNft.sol
@@ -5,6 +5,7 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+import {IMythToken} from "./interfaces/IMythToken.sol";
 
 error MythNft__RangeOutOfBounds();
 error MythNft__NotEnoughTokensPaid();
