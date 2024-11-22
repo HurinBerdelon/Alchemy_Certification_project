8801716ab7b5b47a0eb0da5638a1620bc2f991aa
Fernando Cardozo <fhpcardozo@gmail.com>
2024-11-22 16:06:37 -0300

    feat: add 100% coverage tests for MythToken Contract

--- test/unit/mythToken.spec.ts
+++ test/unit/mythToken.spec.ts
@@ -4,59 +4,170 @@ import { expect } from "chai"
 import MythTokenModule from "../../ignition/modules/MythToken"
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers"
 import { Contract } from "ethers"
+import { MythToken } from "../../typechain-types"
 
 describe("Myth Token Contract", () => {
     let deployer: HardhatEthersSigner
-    let secondUser: HardhatEthersSigner
+    let user: HardhatEthersSigner
+    let externalContract: HardhatEthersSigner
+    let sellerUser: HardhatEthersSigner
     let mythToken: Contract
+    let fundUser: (user?: HardhatEthersSigner, amount?: number) => Promise<{ userBalance: number }>
     const AMOUNT = 500
 
     beforeEach(async () => {
         const accounts = await ethers.getSigners()
         deployer = accounts[0]
-        secondUser = accounts[1]
+        user = accounts[1]
+        externalContract = accounts[2]
+        sellerUser = accounts[3]
 
         const { contract: _mythToken } = await ignition.deploy(MythTokenModule)
         mythToken = _mythToken
+
+        fundUser = async (_user = user, amount = AMOUNT) => {
+            await mythToken.mintForUser(_user, amount)
+
+            const userBalance = await mythToken.balanceOf(_user)
+
+            return { userBalance: Number(userBalance) }
+        }
     })
 
-    it("should construct contract with correct name, symbol", async () => {
-        const name = await mythToken.name()
-        const symbol = await mythToken.symbol()
+    describe("Constructor", () => {
+        it("should construct contract with correct name, symbol", async () => {
+            const name = await mythToken.name()
+            const symbol = await mythToken.symbol()
 
-        expect(name).equals("Myth Token Coin")
-        expect(symbol).equals("MTC")
+            expect(name).equals("Myth Token Coin")
+            expect(symbol).equals("MTC")
+        })
+
+        it("should mint initialSupply tokens to the contract", async () => {
+            const totalSupply = await mythToken.totalSupply()
+            const balanceOfOwner = await mythToken.balanceOf(mythToken.getAddress())
+
+            expect(totalSupply.toString()).equals((1e9).toString())
+            expect(balanceOfOwner.toString()).equals(totalSupply.toString())
+        })
     })
 
-    it("should mint tokens for the deployer", async () => {
-        const totalSupply = await mythToken.totalSupply()
-        const balanceOfOwner = await mythToken.balanceOf(deployer)
+    describe("mintForUser", () => {
+        it("should transfer an amount to an user", async () => {
+            await fundUser()
 
-        expect(totalSupply.toString()).equals((1e9).toString())
-        expect(balanceOfOwner.toString()).equals(totalSupply.toString())
+            const totalSupply = await mythToken.totalSupply()
+            const balanceOfContract = await mythToken.balanceOf(mythToken.getAddress())
+            const balanceOfUser = await mythToken.balanceOf(user)
+
+            expect(Number(balanceOfContract)).equals(Number(totalSupply) - AMOUNT)
+            expect(Number(balanceOfUser)).equals(AMOUNT)
+        })
     })
 
-    it("should transfer an amount to an user", async () => {
-        await mythToken.approve(deployer, AMOUNT)
-        await mythToken.transferFrom(deployer, secondUser, AMOUNT)
+    describe("handleMintNft", () => {
+        it("should be able to spend tokens from user and assign to the contract when minting a NFT", async () => {
+            const mintFee = AMOUNT / 2
+            await fundUser()
 
-        const totalSupply = await mythToken.totalSupply()
-        const balanceOfOwner = await mythToken.balanceOf(deployer)
-        const balanceOfUser = await mythToken.balanceOf(secondUser)
+            const mythTokenMinter = mythToken.connect(user) as MythToken
+            mythTokenMinter.approve(externalContract, mintFee)
 
-        expect(balanceOfOwner.toString()).equals((totalSupply - balanceOfUser).toString())
-        expect(balanceOfUser.toString()).equals(AMOUNT.toString())
+            const mythTokenExternal = mythToken.connect(externalContract) as MythToken
+            await mythTokenExternal.handleMintNFT(user, mintFee)
+
+            const totalSupply = await mythToken.totalSupply()
+            const balanceOfContract = await mythToken.balanceOf(mythToken.getAddress())
+            const balanceOfUser = await mythToken.balanceOf(user)
+
+            expect(Number(balanceOfContract)).equals(Number(totalSupply) - AMOUNT + mintFee)
+            expect(Number(balanceOfUser)).equals(AMOUNT - mintFee)
+        })
+
+        it("should not be able to spend tokens to mint an NFT if user has not enough balance", async () => {
+            const mintFee = AMOUNT / 2
+            const mythTokenMinter = mythToken.connect(user) as MythToken
+            mythTokenMinter.approve(externalContract, mintFee)
+
+            const mythTokenExternal = mythToken.connect(externalContract) as MythToken
+
+            await expect(
+                mythTokenExternal.handleMintNFT(user, mintFee)
+            ).to.be.revertedWithCustomError(mythTokenExternal, "MythToken__NotEnoughToken")
+        })
     })
 
-    it("should get owner", async () => {
-        const owner = await mythToken.getOwner()
+    describe("handleBuy", () => {
+        it("should be able to spend tokens to buy an NFT", async () => {
+            const PRICE = AMOUNT / 2
+            const { userBalance: buyerBalanceBeforePurchase } = await fundUser(user)
 
-        expect(owner).equals(deployer)
+            const mythTokenBuyer = mythToken.connect(user) as MythToken
+            mythTokenBuyer.approve(externalContract, PRICE)
+
+            const mythTokenExternal = mythToken.connect(externalContract) as MythToken
+            await mythTokenExternal.handleBuy(user, sellerUser, PRICE)
+
+            const buyerBalanceAfterPurchase = await mythToken.balanceOf(user)
+
+            expect(Number(buyerBalanceAfterPurchase)).equals(buyerBalanceBeforePurchase - PRICE)
+        })
+
+        it("should set seller's proceeds after handleBuy", async () => {
+            const PRICE = AMOUNT / 2
+            await fundUser(user)
+
+            const mythTokenBuyer = mythToken.connect(user) as MythToken
+            mythTokenBuyer.approve(externalContract, PRICE)
+
+            const mythTokenExternal = mythToken.connect(externalContract) as MythToken
+            await mythTokenExternal.handleBuy(user, sellerUser, PRICE)
+
+            const mythTokenSeller = mythToken.connect(sellerUser) as MythToken
+            const sellerProceeds = await mythTokenSeller.getProceeds()
+
+            expect(Number(sellerProceeds)).equals(PRICE)
+        })
+
+        it("should not buy item if buyer does not have enough funds", async () => {
+            const PRICE = AMOUNT / 2
+
+            const mythTokenBuyer = mythToken.connect(user) as MythToken
+            mythTokenBuyer.approve(externalContract, PRICE)
+
+            const mythTokenExternal = mythToken.connect(externalContract) as MythToken
+
+            await expect(
+                mythTokenExternal.handleBuy(user, sellerUser, PRICE)
+            ).to.be.revertedWithCustomError(mythTokenExternal, "MythToken__NotEnoughToken")
+        })
     })
 
-    it("should be able to transfer tokens from owner to an user", () => {})
+    describe("withdrawProceeds", () => {
+        it("should allow seller to withdraw proceeds", async () => {
+            const PRICE = AMOUNT / 2
+            await fundUser(user)
 
-    it("should be able to remove tokens from user and assign to owner when minting a NFT", async () => {
-        await mythToken.handleMint(secondUser, AMOUNT)
+            const mythTokenBuyer = mythToken.connect(user) as MythToken
+            mythTokenBuyer.approve(externalContract, PRICE)
+
+            const mythTokenExternal = mythToken.connect(externalContract) as MythToken
+            await mythTokenExternal.handleBuy(user, sellerUser, PRICE)
+
+            const mythTokenSeller = mythToken.connect(sellerUser) as MythToken
+            await mythTokenSeller.withdrawProceeds()
+            const sellerBalance = await mythTokenSeller.balanceOf(sellerUser)
+
+            expect(Number(sellerBalance)).equals(PRICE)
+        })
+
+        it("should not allow seller to withdraw zero proceeds", async () => {
+            const mythTokenSeller = mythToken.connect(sellerUser) as MythToken
+
+            await expect(mythTokenSeller.withdrawProceeds()).to.be.revertedWithCustomError(
+                mythTokenSeller,
+                "MythToken__NoProceeds"
+            )
+        })
     })
 })
