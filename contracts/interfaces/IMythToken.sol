e605be524a075487b8a9e391a33016aa86bc3b51
Fernando Cardozo <fhpcardozo@gmail.com>
2024-11-22 16:06:15 -0300

    feat: update MythToken contract to safe funds in the contract address instead of contract owner

--- contracts/interfaces/IMythToken.sol
+++ contracts/interfaces/IMythToken.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.27;
 
 interface IMythToken {
+    function mintForUser(address to, uint256 value) external;
+
     function handleMintNFT(address minter, uint256 value) external returns (bool);
 
     function handleBuy(address buyer, address seller, uint256 value) external returns (bool);
 
-    function withdrawProceeds() external;
+    function withdrawProceeds() external returns (bool);
 
-    function getProceeds(address seller) external returns (uint256);
+    function getProceeds() external returns (uint256);
 }
